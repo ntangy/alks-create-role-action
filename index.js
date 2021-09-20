165ba1a364bdc216bc6ad98b9e8f034652f8d020
Nick Tang <nick.tang@coxautoinc.com>
2021-09-20 11:38:39 -0400

    update action.yml to include sts creds

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
+try {
+    const accessKeyId = core.getInput('aws-access-key-id', { required: false });
+    const secretAccessKey = core.getInput('aws-secret-access-key', { required: false });
+    const sessionToken = core.getInput('aws-session-token', { required: false });
+    const roleType = core.getInput('role-type', { required: true });
+    const roleName = core.getInput('role-name', { required: true });
+
+
+} catch (error) {
+    core.setFailed(error.message)
+}
