6222e1c7d495a7b70169470205b29c24d1863000
Nick Tang <nick.tang@coxautoinc.com>
2021-09-20 13:52:42 -0400

    call into alks loginrole for identification

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const ALKS = require('alks.js');
+
+const ALKS_BASE_URL = 'https://alks.coxautoinc.com/rest';
 
 
-try {
-    const accessKeyId = core.getInput('aws-access-key-id', { required: false });
-    const secretAccessKey = core.getInput('aws-secret-access-key', { required: false });
-    const sessionToken = core.getInput('aws-session-token', { required: false });
-    const roleType = core.getInput('role-type', { required: true });
-    const roleName = core.getInput('role-name', { required: true });
+async function run() {
+    try {
+        const accessKeyId = core.getInput('aws-access-key-id', { required: false });
+        const secretAccessKey = core.getInput('aws-secret-access-key', { required: false });
+        const sessionToken = core.getInput('aws-session-token', { required: false });
+        const roleType = core.getInput('role-type', { required: true });
+        const roleName = core.getInput('role-name', { required: true });
+    
+        const alks = ALKS.create({
+            baseUrl: ALKS_BASE_URL,
+            accessKey: accessKeyId,
+            secretKey: secretAccessKey,
+            sessionToken: sessionToken,
+        });
+        // infer acc info form STS creds can do by calling alks loginrole/id/me no alks.js do it directly
+        const response = await fetch(ALKS_BASE_URL + "/loginRoles/id/me", {
+            method: 'GET',
+            headers: {
+                'ALKS-STS-Access-Key': accessKeyId,
+                'ALKS-STS-Secret-Key': secretAccessKey,
+                'ALKS-STS-Session-Token': sessionToken,
+            }
+        });
+        if(!response.ok) {
+            throw new Error('Could not validate STS credentials/could not be recognized by ALKS')
+        }
+        const data = await response.json();
+        const account = data.loginRole.account;
+        const role = data.loginRole.role;
+        const alias = data.loginRole.skypieaAccount.alias;
 
-
-} catch (error) {
-    core.setFailed(error.message)
+        console.log(account, role, alias);
+    
+        // let createdRole = await alks.createRole({
+        // });
+    
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
+
+run();
